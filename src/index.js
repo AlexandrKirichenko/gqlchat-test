fc7cc7e8c72cb7d87affeb0364c5dd71d63da98e
VladimirDiamandi <vladdiamandi@gmail.com>
2021-09-02 17:58:35 +0300

    init app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+} from "@apollo/client";
+
+    const client = new ApolloClient({
+        uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        cache: new InMemoryCache()
+    });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+// function App() {
+//     const client = new ApolloClient({
+//         uri: 'https://48p1r2roz4.sse.codesandbox.io',
+//         cache: new InMemoryCache()
+//     });
+//
+
+//
+//     function ExchangeRates() {
+//         const { loading, error, data } = useQuery(EXCHANGE_RATES);
+//
+//         if (loading) return <p>Loading...</p>;
+//         if (error) return <p>Error :(</p>;
+//
+//         return data.rates.map(({ currency, rate }) => (
+//             <div key={currency}>
+//                 <p>
+//                     {currency}: {rate}
+//                 </p>
+//             </div>
+//         ));
+//     }
+//
+//     return (
+//         <div>
+//             <h2>My first Apollo app 🚀</h2>
+//             <ExchangeRates />
+//         </div>
+//     );
+// }
+//
+// export default App;
